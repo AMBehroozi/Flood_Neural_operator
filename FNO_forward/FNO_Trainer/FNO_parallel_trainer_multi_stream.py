36448c0072c1329de6718d53be18d473dd71508c
AMBehroozi <behroozi.fx@gmail.com>
2026-01-30 09:49:23 -0500

    add stage 3

--- FNO_forward/FNO_Trainer/FNO_parallel_trainer_multi_stream.py
+++ FNO_forward/FNO_Trainer/FNO_parallel_trainer_multi_stream.py
@@ -585,11 +585,13 @@ if __name__ == "__main__":
     # Dam break
     # cfg = load_config('FNO_forward/FNO_Trainer/configs/dam_break_config_stage1.yml')    
     # cfg = load_config('FNO_forward/FNO_Trainer/configs/dam_break_config_stage2.yml')    
-    
+    cfg = load_config('FNO_forward/FNO_Trainer/configs/dam_break_config_stage3.yml')    
+
     # flooding
     # cfg = load_config('FNO_forward/FNO_Trainer/configs/flooding_config_stage1.yml')
-    cfg = load_config('FNO_forward/FNO_Trainer/configs/flooding_config_stage2.yml')
-    
+    # cfg = load_config('FNO_forward/FNO_Trainer/configs/flooding_config_stage2.yml')
+    # cfg = load_config('FNO_forward/FNO_Trainer/configs/flooding_config_stage2.yml')
+   
     run_nvidia_smi()
     MHPI()
     warnings.filterwarnings("ignore", message="incompatible copy of pydevd already imported")
